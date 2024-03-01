169c38f5dae7e459504f71c4e0905d6eb7b797e1
sabocaisa <cardosoisa160@gmail.com>
2024-03-01 10:58:30 -0300

    codigo inicial

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$login_cookie = $_COOKIE['login'];
+if(isset($login_cookie)){
+    echo"Be-Vindo, $login_cookie <br>";
+    echo"Essas informaçoes <font color='red'> PODEM </font> ser acessadas por voce";
+}else{
+    echo"Bem-Vindo, convidado <br>";
+    echo"Essas informaçoes <font color='red'> NAO PODEM </font> ser acessadas por voce";
+    echo"<br><a href='login.html'>Faça login</a> Para ler o conteudo";
+}
+?>
